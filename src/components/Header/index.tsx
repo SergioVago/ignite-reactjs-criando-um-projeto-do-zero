2641b3f2a07d4b72f6d2b5c117035d0f1122fa96
Sergio Vago <sergio.vago@hotmail.com>
2021-08-17 11:12:08 -0300

    Criação do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,29 @@
-export default function Header() {
-  // TODO
+import { useRouter } from 'next/router';
+import { useCallback } from 'react';
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+interface HeaderProps {
+  home?: boolean;
+}
+
+export default function Header({ home }: HeaderProps) {
+  const router = useRouter();
+
+  const handleGoHome = useCallback(() => {
+    router.push('/', '/', {});
+  }, [router]);
+
+  return (
+    <header className={`${commonStyles.container} ${home && styles.home}`}>
+      <div className={styles.header}>
+        <img
+          src="/images/logo.svg"
+          alt="logo"
+          onClick={handleGoHome}
+          role="presentation"
+        />
+      </div>
+    </header>
+  );
 }
